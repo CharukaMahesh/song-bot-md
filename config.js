94b4a8779b41f2721e89326981a0c8b900988d69
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-22 01:43:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,11 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/CharukaMahesh/QUEEN-CHETHI/refs/heads/main/IMGES/20240921_115553.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "*`HI I AM ONLINE...🪄`*",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+    SESSION_ID: process.env.SESSION_ID || 'default-session-id', // Add a default value if session ID is not provided
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/CharukaMahesh/QUEEN-CHETHI/refs/heads/main/IMGES/20240921_115553.png",
+    ALIVE_MSG: process.env.ALIVE_MSG || "*`HI I AM ONLINE...🪄`*",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    MODE: process.env.MODE || "public",
 };
