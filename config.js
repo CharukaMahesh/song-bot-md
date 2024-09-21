8148c1e8a0151bb944a0d5993fcaa952467e634a
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-21 12:19:12 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/CharukaMahesh/QUEEN-CHETHI/refs/heads/main/IMGES/20240921_115553.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "*`HI I AM ONLINE...🪄`*",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+};
