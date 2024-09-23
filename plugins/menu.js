76dfd1f05ad1de929c951676737fefc359eb6998
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-23 18:43:13 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,36 @@
+const { cmd } = require('../command');
+
+cmd({
+    pattern: "menu",
+    desc: "Displays bot menu",
+    category: "tools",
+    filename: __filename
+},
+async (conn, mek, m, { from }) => {
+    const caption = `*◈╾───❄️ꜱᴏɴɢ ʙᴏᴛ❄️───╼◈*
+
+• *\`ꜱᴏɴɢ\`* - ᴅᴏᴡɴʟᴏᴀᴅ ꜱᴏɴɢ ꜰʀᴏᴍ ʏᴏᴜᴛᴜʙᴇ..❄️
+• *\`ᴠɪᴅᴇᴏ\`* - ᴅᴏᴡɴʟᴏᴀᴅ ᴠɪᴅᴇᴏ ꜰʀᴏᴍ ʏᴏᴜᴛᴜʙᴇ..❄️
+• *\`ʏᴛꜱᴇᴀʀᴄʜ\`* - ꜱᴇᴀʀᴄʜ ᴀᴄʀᴏꜱꜱ ʏᴏᴜᴛᴜʙᴇ..❄️
+• *\`ᴀʟɪᴠᴇ\`* - ᴄʜᴇᴄᴋ ʙᴏᴛ ᴏɴʟɪɴᴇ ᴏʀ ɴᴏ..❄️
+• *\`ᴘɪɴɢ\`* - ᴄʜᴇᴄᴋ ʙᴏᴛ ꜱᴘᴇᴇᴅ..❄️
+
+*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʜᴀʀᴜᴋᴀ ᴍᴀʜᴇꜱʜ*
+
+*ᴛʜᴀɴᴋꜱ ꜰᴏʀ:*
+❄️ ᴜᴍᴇꜱʜᴀ ꜱᴀᴛʜʏᴀɴᴊᴀʟɪ
+❄️ ᴍɪᴛʜɪʟᴀ ꜱʜᴀʀᴀᴅʜᴀ
+❄️ɴᴀᴠᴇᴇɴ ᴘᴇᴛʜᴜᴍ
+❄️ᴄʜᴇᴛʜᴀɴᴀ ʀᴀᴊᴀɢᴜʀᴜ`;
+
+    const image = 'https://raw.githubusercontent.com/CharukaMahesh/song-bot-md/refs/heads/main/IMGES/20240923_144904.jpg';
+
+    const buttonMessage = {
+        image: { url: image },
+        caption: caption,
+        footer: 'Bot Menu',
+        headerType: 4
+    };
+
+    await conn.sendMessage(from, buttonMessage);
+});
